ca5baaa96f7e2e808c9ada7e31c8574c9b6c205c
Christo Smal <christo.smal@ionize.co.za>
2021-09-20 17:17:08 +0200

    Implemented getArnieQuotes method

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,16 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+    const proms = urls.map(url => httpGet(url))
+    const responses = await Promise.allSettled(proms)
+    const res = responses.map(httpResp => {
+        const key = httpResp.status === 'fulfilled' && httpResp.value.status === 200 ? 'Arnie Quote' : 'FAILURE'
+        const body = JSON.parse(httpResp.value.body)
+        return {
+            [key] : body.message
+        }
+    })
+    return res
 };
 
 module.exports = {
